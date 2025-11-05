8001f62f1641c0ba8d151b3c85d8742983dc183e
himoza <impocanal@gmail.com>
2025-11-05 18:50:45 +0500

    correction in titanic_analysis.py

--- titanic_analysis.py
+++ titanic_analysis.py
@@ -13,21 +13,27 @@ def analyze_titanic_data(df, survived, number):
         DataFrame с результатами
     """
     if number:
-        res_male = df[(df['Sex'] == 'male') & (df['Survived'] == survived)].shape[0]
-        res_female = df[(df['Sex'] == 'female') & (df['Survived'] == survived)].shape[0]
+        res_male = \
+            df[(df['Sex'] == 'male') & (df['Survived'] == survived)].shape[0]
+        res_female = \
+            df[(df['Sex'] == 'female') & (df['Survived'] == survived)].shape[0]
 
         result_df = pd.DataFrame({
             'Пол': ['male', 'female'],
             'Число': [res_male, res_female]
         })
     else:
-        res_male = df[(df['Sex'] == 'male') & (df['Survived'] == survived)].shape[0]
+        res_male = \
+            df[(df['Sex'] == 'male') & (df['Survived'] == survived)].shape[0]
         male_count = df[df['Sex'] == 'male'].shape[0]
-        res_female = df[(df['Sex'] == 'female') & (df['Survived'] == survived)].shape[0]
+        res_female = \
+            df[(df['Sex'] == 'female') & (df['Survived'] == survived)].shape[0]
         female_count = df[df['Sex'] == 'female'].shape[0]
 
-        male_percent = round(res_male / male_count * 100, 2) if male_count > 0 else 0
-        female_percent = round(res_female / female_count * 100, 2) if female_count > 0 else 0
+        male_percent = round(res_male / male_count * 100, 2) \
+            if male_count > 0 else 0
+        female_percent = round(res_female / female_count * 100, 2) \
+            if female_count > 0 else 0
 
         result_df = pd.DataFrame({
             'Пол': ['male', 'female'],
